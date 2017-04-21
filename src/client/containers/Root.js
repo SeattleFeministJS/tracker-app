a7a56ca04fc7c148029f555e1705e7461a479f7c
simplesthing <i.am.simplesthing@gmail.com>
2017-04-20 19:59:19 -0700

    connect Root

--- src/client/containers/Root.js
+++ src/client/containers/Root.js
@@ -1,13 +1,25 @@
 import App from '../components/App/'
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Root extends Component {
+class Root extends Component {
 
   constructor(props) {
     super(props)
   }
 
   render() {
+    console.log(this.props)
     return <App>{this.props.children}</App>
   }
 }
+
+
+function mapStateToProps(state) {
+  return {
+    app: state.app
+  }
+}
+
+
+export default connect(mapStateToProps)(Root)
